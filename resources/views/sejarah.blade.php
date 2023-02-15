93457001ea63217e2d2b7c31f1c62c2b196aae52
wirbn <wbernandi@gmail.com>
2023-02-15 19:34:01 +0700

    feat: Update Lagi dan Lagi

--- resources/views/sejarah.blade.php
+++ resources/views/sejarah.blade.php
@@ -110,19 +110,19 @@
           </div>
           <a href="#" class="ng text-center">Gereja Segala Bangsa Malinau Kota</a>
           
-          <div class="col-md-3 mt-3 d-flex align-items-center justify-content-evenly">
+          <div class="papa col-md-3 mt-3 d-flex align-items-center justify-content-evenly">
             <a href="https://www.instagram.com/gesbamalkot/" target="_blank"><i class="bi bi-instagram"></i></a>
             <a href="https://www.youtube.com/channel/UCukPPZ6Gr-lpiwjrl-EJlWw" target="_blank"><i class="bi bi-youtube"></i></a>
             <a href="https://www.facebook.com/people/GESBA-Malinau-Kota/100088256063650/?mibextid=ZbWKwL" target="_blank"><i class="bi bi-facebook"></i></a>
             <a href="#hubungi" target="_blank"><i class="bi bi-envelope-fill"></i></a>
           </div>
-          <div class="ala mt-5 col-md-5 d-flex">
+          {{-- <div class="ala mt-5 col-md-5 d-flex">
             <a href="#" class="alam ms-3">GESBA Malinau Kota Jl. Pasar Induk Gang Gesba RT.20, Kec. Malinau Kota, Kabupaten Malinau, Kalimantan Utara 77554</a>
-          </div>
+          </div> --}}
         </div>
         <hr class="grs">
         <div class="row position-absolute copyright start-50 translate-middle">
-          <div class="col-12">
+          <div class="cp col-12">
             <p>Copyright by Yusri Mandi Pakiding All Right Reserved.</p>
           </div>
         </div>
